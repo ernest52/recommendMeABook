150f77121c605c5efeee53e165d0a2eccae2acda
Ernest Zet <ernest52@onet.pl>
2024-08-19 13:55:04 +0200

    secure db data

--- pgConnection/db.js
+++ pgConnection/db.js
@@ -1,9 +1,13 @@
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
 const db = new pg.Client({
-  user: "postgres",
-  database: "recomendMeABook",
-  port: 5432,
-  password: "gyhdEede3RDeede3",
+  user: process.env.PG_USER,
+  database: process.env.PG_DB,
+  port: process.env.PG_PORT,
+  password: process.env.PG_PASSWORD,
 });
+
 db.connect();
 export default db;
